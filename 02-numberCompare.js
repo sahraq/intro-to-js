45cfd004b45f4e56908d6d15863869baa7662aab
sahraq <soq204@nyu.edu>
2024-12-14 12:20:26 -0500

    Create 02-numberCompare.js

---
+++ 02-numberCompare.js
@@ -0,0 +1,10 @@
+let num1 = 0;
+let num2 = 1;
+
+if (num1 > num2) {
+    alert("The first value is larger.");
+} else if (num1 < num2) {
+    alert("The second value is larger.");
+} else {
+    alert("Both values are the same.");
+}
